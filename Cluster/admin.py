91128dbcecf6e84e35e39c6c10730f85ab16499c
pacebrian0 <pacebrian0@gmail.com>
2018-02-08 08:48:22 +0100

    Added models and admin

--- Cluster/admin.py
+++ Cluster/admin.py
@@ -1,1 +1,53 @@
 # Register your models here.
+from django.contrib import admin
+
+from .models import *
+
+
+class userAdmin(admin.ModelAdmin):
+    fieldsets = [
+        ("ID", {'fields': ['number']}),
+        ("String", {'fields': ['string']}),
+        ("Age Range", {'fields': ['agerange']}),
+    ]
+
+    list_display = ('number', 'string', 'agerange')
+    list_filter = ['agerange', ]
+    search_fields = ['number', 'agerange']
+
+
+class historyAdmin(admin.ModelAdmin):
+    fieldsets = [
+        ("User", {'fields': ['user']}),
+        ("Design Number", {'fields': ['designnumber']}),
+        ("URL", {'fields': ['url']}),
+        ("Clusters", {'fields': ['clusters']}),
+    ]
+    list_display = ('user', 'designnumber', 'url', 'clusters',)
+    list_filter = ['designnumber', 'user']
+    search_fields = ['user', 'designnumber', 'url']
+
+
+class statsAdmin(admin.ModelAdmin):
+    fieldsets = [
+        ("User", {'fields': ['user']}),
+        ("Time Start Design 1", {'fields': ['timestartd1']}),
+        ("Time End Design 1", {'fields': ['timeendd1']}),
+        ("Time Delta Design 1", {'fields': ['timedeltad1']}),
+        ("Time Start Design 2", {'fields': ['timestartd2']}),
+        ("Time End Design 2", {'fields': ['timeendd2']}),
+        ("Time Delta Design 2", {'fields': ['timedeltad2']}),
+        ("Time Start Design 3", {'fields': ['timestartd3']}),
+        ("Time End Design 3", {'fields': ['timeendd3']}),
+        ("Time Delta Design 3", {'fields': ['timedelta3']}),
+
+    ]
+    list_display = ('user', 'timedeltad1', 'timedeltad2', 'timedeltad3')
+    list_filter = ['user']
+    search_fields = ['user', 'timestartd1', 'timeendd1', 'timedeltad1', 'timestartd2', 'timeendd2', 'timedeltad2',
+                     'timestartd3', 'timeendd3', 'timedeltad3']
+
+
+admin.site.register(User, userAdmin)
+admin.site.register(History, historyAdmin)
+admin.site.register(Stats, statsAdmin)
--- Cluster/models.py
+++ Cluster/models.py
@@ -1,17 +1,39 @@
 from django.db import models
 
 
+# TODO: ADD ENTRIES FROM QUESTIONNAIRE
+
+
 class User(models.Model):
-    number = models.PositiveSmallIntegerField(max_length=200, default="")
-    string = models.CharField(max_length=200, default="")
+    number = models.PositiveSmallIntegerField("User Identifier", default=1)
+    string = models.CharField("String", max_length=200, default="")
+    agerange = models.PositiveSmallIntegerField("Age Range", default=0)
 
     def __str__(self):
         return self.string
 
 
 class History(models.Model):
-    name = models.ForeignKey(User, on_delete=models.CASCADE)
-    historyurl = models.CharField(max_length=200, default="")
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    url = models.CharField("URL", max_length=200, default="", blank=True)
+    designnumber = models.PositiveSmallIntegerField("Design", default=0, blank=True)
+    clusters = models.FilePathField("Clusters", blank=True)
 
     def __str__(self):
-        return self.name
+        return self.user
+
+
+class Stats(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    timestartd1 = models.DateTimeField("Start time design 1", blank=True)
+    timeendd1 = models.DateTimeField("End time design 1", blank=True)
+    timedeltad1 = models.DateTimeField("Delta time design 1", blank=True)
+    timestartd2 = models.DateTimeField("Start time design 2", blank=True)
+    timeendd2 = models.DateTimeField("End time design 2", blank=True)
+    timedeltad2 = models.DateTimeField("Delta time design 2", blank=True)
+    timestartd3 = models.DateTimeField("Start time design 3", blank=True)
+    timeendd3 = models.DateTimeField("End time design 3", blank=True)
+    timedeltad3 = models.DateTimeField("Delta time design 3", blank=True)
+
+    def __str__(self):
+        return self.user
